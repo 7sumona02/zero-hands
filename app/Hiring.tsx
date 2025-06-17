8f6720d572f1bd637eea6dd8014d5cb7aba1d0f6
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-06-17 19:45:44 +0530

    added

--- app/Hiring.tsx
+++ app/Hiring.tsx
@@ -1,9 +1,18 @@
 'use client'
 import React, { useState } from 'react'
-import HiringCard from './HiringCard'
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { Input } from '@/components/ui/input'
 import { X } from 'lucide-react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import HiringCard from './HiringCard'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 
 interface HiringItem {
   id: number;
@@ -42,8 +51,8 @@ const Hiring = () => {
   };
 
   return (
-    <div className='bg-white min-h-screen w-screen'>
-      <div className='container py-10 max-w-3xl mx-auto text-black'>
+    <div className='bg-white min-h-screen w-full'>
+      <div className='container md:px-0 px-6 py-10 max-w-3xl mx-auto text-black'>
         <div>
           <div className="*:not-first:mt-2 pb-10">
             <div className="relative">
@@ -65,7 +74,7 @@ const Hiring = () => {
           </div>
         </div>
         
-        <ScrollArea className="h-[612px] max-w-3xl">
+        {/* <ScrollArea className="h-[612px] max-w-3xl">
           {filteredItems.length > 0 ? (
             filteredItems.map(item => (
               <HiringCard 
@@ -80,6 +89,29 @@ const Hiring = () => {
               No results found for "{searchTerm}"
             </div>
           )}
+        </ScrollArea> */}
+        <ScrollArea className="h-[612px] max-w-3xl md:pb-0 pb-5">
+          <Table>
+            <TableBody>
+              <TableRow className='bg-transparent hover:bg-transparent'>
+              {filteredItems.length > 0 ? (
+            filteredItems.map(item => (
+              <HiringCard 
+                key={item.id}
+                name={item.name} 
+                title={item.title} 
+                date={item.date} 
+              />
+            ))
+          ) : (
+            <div className="text-center py-10 text-muted-foreground">
+              No results found for "{searchTerm}"
+            </div>
+          )}
+              </TableRow>
+            </TableBody>
+          </Table>
+          <ScrollBar orientation='horizontal' />
         </ScrollArea>
       </div>
     </div>
